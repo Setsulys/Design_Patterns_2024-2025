6b5623349e69c3d8445d8bf2d04a389802360921
STEVEN LY <steven.ly@nomachine.univ-eiffel.fr>
2024-09-23 11:16:03 +0200

    TP1 ex1

--- TP1/src/fr/uge/poo/simplegraphics/SimpleGraphicsExample.java
+++ TP1/src/fr/uge/poo/simplegraphics/SimpleGraphicsExample.java
@@ -9,8 +9,8 @@ public class SimpleGraphicsExample {
     private static void drawAll(Graphics2D graphics) {
         graphics.setColor(Color.BLACK);
         graphics.drawRect(100, 20, 40, 140);
-        graphics.drawLine(0, 0, 800, 800);
-        graphics.drawLine(0, 800, 800, 0);
+        graphics.drawLine(0, 0, 800, 600);
+        graphics.drawLine(0, 600, 800, 0);
         graphics.drawOval(100,20,40,140);
     }
 
