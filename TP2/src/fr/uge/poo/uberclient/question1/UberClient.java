ab836939e4cd2d687748590d6e96dfc92146755c
LY-IENG Steven <steven.ly412@gmail.com>
2024-10-11 17:57:01 +0200

    continued tp

--- TP2/src/fr/uge/poo/uberclient/question1/UberClient.java
+++ TP2/src/fr/uge/poo/uberclient/question1/UberClient.java
@@ -1,92 +1,116 @@
 package fr.uge.poo.uberclient.question1;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.ThreadLocalRandom;
 
 public class UberClient {
 
-	   private final String firstName;
-	   private final String lastName;
-	   private final long uid;
-	   private final List<Integer> grades;
-	   private final List<String> emails;
-	   private final List<String> phoneNumbers;
-	   
-	   private UberClient(String firstName, String lastName, List<Integer> grades, List<String> emails, List<String> phoneNumbers) {
-	       this.firstName = firstName;
-	       this.lastName= lastName;
-	       this.uid = ThreadLocalRandom.current().nextLong(0,Long.MAX_VALUE);
-	       this.grades = List.copyOf(grades);
-	       this.emails = List.copyOf(emails);
-	       this.phoneNumbers = List.copyOf(phoneNumbers);
-	   }
-	   
-	   private UberClient(String firstName, String lastName,long uid, List<Integer> grades, List<String> emails, List<String> phoneNumbers) {
-		   this.firstName = firstName;
-	       this.lastName= lastName;
-	       this.uid = uid;
-	       this.grades = List.copyOf(grades);
-	       this.emails = List.copyOf(emails);
-	       this.phoneNumbers = List.copyOf(phoneNumbers);
-	   }
-	   
-	   public static class Builder{
-		   private String firstName;
-		   private String lastName;
-		   private long uid;
-		   private List<Integer> grades;
-		   private List<String> emails;
-		   private List<String> phoneNumbers;
-		   
-		   public Builder firstName(String firstName) {
-			   this.firstName = Objects.requireNonNull(firstName);
-			   return this;
-		   }
-		   
-		   public Builder lastName(String lastName) {
-			   this.lastName = Objects.requireNonNull(lastName);
-			   return this;
-		   }
-		   
-		   public Builder uid(long uid) {
-			   this.uid = uid;
-			   return this;
-		   }
-		   
-		   public Builder grades(List<Integer> grades) {
-			   this.grades = List.copyOf(grades);
-		       for(var grade : grades){
-		    	   if (grade < 1 ||grade > 5) {
-		    		   throw new IllegalArgumentException("All grades must be between 1 and 5");
-		    	   }
-		       }
-	           return this;
-		   }
-		   
-		   public Builder email(List<String> emails) {
-			   this.emails = List.copyOf(emails);
-		       if (emails.size()==0 && phoneNumbers.size()==0) {
-		           throw new IllegalArgumentException("A client must have at least an email or a phoneNumber");
-		       }
-			   return this;
-		   }
-		   
-		   public Builder phoneNumbers(List<String> phoneNumbers) {
-			   this.phoneNumbers = List.copyOf(phoneNumbers);
-		       if (phoneNumbers.size()==0){
-		           throw new IllegalArgumentException("A client must have at least one grade");
-		       }
-		       return this;
-		   }
-		   
-		   public UberClient build() {
-			   if (firstName == null || lastName == null){
-	               throw new IllegalStateException();
-	           }
-	           return new UberClient(firstName,lastName,grades,emails,phoneNumbers);
-		   }
-	   }
+	private final String firstName;
+	private final String lastName;
+	private final long uid;
+	private final List<Integer> grades;
+	private final List<String> emails;
+	private final List<String> phoneNumbers;
 
+	private UberClient(String firstName, String lastName, List<Integer> grades, List<String> emails, List<String> phoneNumbers) {
+		this.firstName = firstName;
+		this.lastName= lastName;
+		this.uid = ThreadLocalRandom.current().nextLong(0,Long.MAX_VALUE);
+		this.grades = List.copyOf(grades);
+		this.emails = List.copyOf(emails);
+		this.phoneNumbers = List.copyOf(phoneNumbers);
 	}
+
+	private UberClient(String firstName, String lastName,long uid, List<Integer> grades, List<String> emails, List<String> phoneNumbers) {
+		this.firstName = firstName;
+		this.lastName= lastName;
+		this.uid = uid;
+		this.grades = List.copyOf(grades);
+		this.emails = List.copyOf(emails);
+		this.phoneNumbers = List.copyOf(phoneNumbers);
+	}
+
+	public static class Step{
+
+		public static FirstNameStep newBuilder() {
+			return new Builder();
+		}
+		public interface FirstNameStep{
+			public LastNameStep firstName(String firstName);
+		}
+		public interface LastNameStep{
+			public UidStep lastName(String lastName);
+		}
+		public interface UidStep{
+			public GradeStep uid(long uid);
+		}
+		public interface GradeStep{
+			public EmailStep grade(int grades);
+		}
+		public interface EmailStep{
+			public PhoneNumberStep email(String emails);
+		}
+		public interface PhoneNumberStep{
+			public BuildStep phoneNumber(String phoneNumbers);
+		}
+		
+		public interface BuildStep{
+			public UberClient build();
+		}
+
+		public static class Builder implements FirstNameStep, LastNameStep, UidStep, GradeStep, EmailStep, PhoneNumberStep,BuildStep{
+			private String firstName;
+			private String lastName;
+			private long uid;
+			private List<Integer> grades = new ArrayList<>();
+			private List<String> emails= new ArrayList<>();
+			private List<String> phoneNumbers= new ArrayList<>();
+
+			public LastNameStep firstName(String firstName) {
+				this.firstName = Objects.requireNonNull(firstName);
+				return this;
+			}
+
+			public UidStep lastName(String lastName) {
+				this.lastName = Objects.requireNonNull(lastName);
+				return this;
+			}
+
+			public GradeStep uid(long uid) {
+				this.uid = uid;
+				return this;
+			}
+
+			public EmailStep grade(int grade) {
+				if (grade < 1 ||grade > 5) {
+					throw new IllegalArgumentException("All grades must be between 1 and 5");
+				}
+				this.grades.add(grade);
+				return this;
+			}
+
+			public PhoneNumberStep email(String email) {
+				this.emails.add(email);
+				return this;
+			}
+
+			public BuildStep phoneNumber(String phoneNumbers) {
+				this.phoneNumbers.add(phoneNumbers);
+				return this;
+			}
+
+			public UberClient build() {
+				if (firstName == null || lastName == null){
+					throw new IllegalStateException();
+				}
+				if (emails.size()==0 && phoneNumbers.size()==0) {
+					throw new IllegalArgumentException("A client must have at least an email or a phoneNumber");
+				}
+				return new UberClient(firstName,lastName,grades,emails,phoneNumbers);
+			}
+		}   
+	}
+}
 
--- TP2/src/fr/uge/poo/uberclient/question1/main.java
+++ TP2/src/fr/uge/poo/uberclient/question1/main.java
@@ -2,12 +2,15 @@ package fr.uge.poo.uberclient.question1;
 
 import java.util.List;
 
-import fr.uge.poo.uberclient.question1.UberClient.Builder;
+import fr.uge.poo.uberclient.question1.UberClient.Step;
+
 
 public class main {
 	   public static void main(String[] args) {
-	        var arnaud = new UberClient("Arnaud","Carayol",1,List.of(1,2,5,2,5,1,1,1),List.of("arnaud.carayol@univ-eiffel.fr","arnaud.carayol@u-pem.fr"),List.of("07070707070707"));
-	        var youssef = new UberClient("Youssef", "Bergeron", List.of(5), List.of("youssefbergeron@outlook.fr"),List.of());
-	        var yassine = new Builder().firstName("Yassine").lastName("Benmechich").email(List.of("yaskow@hotmail.fr")).grades(List.of(5)).phoneNumbers(List.of("0606060606"));
+	       //var arnaud = new UberClient("Arnaud","Carayol",1,List.of(1,2,5,2,5,1,1,1),List.of("arnaud.carayol@univ-eiffel.fr","arnaud.carayol@u-pem.fr"),List.of("07070707070707"));
+	       //var youssef = new UberClient("Youssef", "Bergeron", List.of(5), List.of("youssefbergeron@outlook.fr"),List.of());
+		   //var yassine = new Builder().firstName("Yassine").lastName("Benmechich").email(List.of("yaskow@hotmail.fr")).grade(List.of(5)).phoneNumber(List.of("0606060606"));
+		   var christophe = Step.newBuilder().firstName("Christophe").lastName("Haidamous").uid(56).grade(4).email("christophe.haidamous@univ-eiffel.fr").phoneNumber("0606060606").build();
+		   System.out.println(christophe);
 	    }
 }
